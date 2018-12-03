993df35c4cd699e4a87facd09f987787405d00a3
wanessamarcondes <wanessamarcondes@gmail.com>
2018-12-03 15:57:38 -0200

    initial commit

---
+++ my-app/src/components/header/header.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Navbar from "./navbar"
+import "./header.css"
+import {Bootstrap} from "react-bootstrap"
+
+const Header = () => (
+    <div className="container-fluid"
+        <Navbar />
+    />
+)
+
+export default Header
---
+++ my-app/src/components/header/navbar/navbar.js
@@ -0,0 +1,5 @@
+import React from "react"
+import {Link} from "react-router-dom"
+import Menu from "./menu"
+import "./navbar.css"
+
